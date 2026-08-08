79bde462699b6efb0282692d09305e1d461c6ad8
Artem <gavr.artem.s@gmail.com>
2026-08-08 19:22:10 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    if command:
+        separete_command = command.split(" ")
+        if len(separete_command) > 2 and separete_command[0] == "cp":
+            file1 = separete_command[1]
+            file2 = separete_command[2]
+            if file1 == file2:
+                return
+            if len(file1) > 0:
+                try:
+                    with (open(file1, "r") as file_out,
+                          open(file2, "w") as file_in):
+                        file_in.write(file_out.read())
+                except FileNotFoundError:
+                    pass
